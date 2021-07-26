1160169292053d26aa6bc2f1dfd050e204a70797
Auguste <auguste.tomaseviciute@gmail.com>
2021-07-26 17:45:05 +0100

    add base app requirements in html

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="statistic">
+      <div id="total">
+        <h2>Total</h2>
+      </div>
+      <div id="completed">
+        <h2>Completed</h2>
+      </div>
+      <div id="not-completed">
+        <h2>Not completed</h2>
+      </div>
+    </div>
+
+    <div class="note-table">
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Content</th>
+          <th>Status</th>
+        </tr>
+      </table>
+    </div>
+
+    <div class="add-note">
+      <button type="button">Add</button>
+    </div>
+
+    <div id="new-note">
+      <h1>Add note</h1>
+      <form action="">
+        <input type="text" name="title" id="title" placeholder="Add title...">
+        <input type="text" name="content" id="content" placeholder="Add your note...">
+        <input type="submit" value="Save">
+        <input type="button" value="Cancel">
+      </form>
+    </div>
+
+    <div class="alert">
+      <span></span>
+      <span>Do you want to delete this note?</span>
+      <button type="button" id="no">No</button>
+      <button type="button" id="yes">Yes</button>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
