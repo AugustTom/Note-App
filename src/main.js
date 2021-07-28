868aefdae4726f12c01229f14ab08f059aead292
Auguste <auguste.tomaseviciute@gmail.com>
2021-07-28 15:23:53 +0100

    Tailwind add

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { makeServer } from "./server"
+import './index.css'
+
+// Vue(App).mount('#app')
 
 if (process.env.NODE_ENV === "development") {
   makeServer()
